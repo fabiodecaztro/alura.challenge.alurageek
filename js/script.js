b73f10f90f8f5f09d97f32ed7e1570ba547b367b
Fabio Amorim <fabiodecaztro@gmail.com>
2022-04-04 21:41:54 -0300

    reposividade HEADER

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// let banner = document.querySelector('.banner');
+// let width = window.screen.width;
+// console.log(width)
+// if(width == 768){
+//     banner.src = 'img/heroTablet.png';
+// }
+
+
